4762d945f5363f8698c5cd2bf062d68b85fd8298
ffukamizu <f.fukamizu@live.com>
2023-04-13 20:40:10 -0300

    fixed issues

--- scripts/script.js
+++ scripts/script.js
@@ -117,9 +117,10 @@ function messageHistoryUpdate() {
   function getLastMessageContent(array) {
     const currentArray = array.data;
 
-    const difference = currentArray.splice(99, 100);
+    const difference = currentArray.filter((n) => !lastArray.includes(n));
 
     if (JSON.stringify(lastArray) !== JSON.stringify(array.data)) {
+      console.log(difference);
       for (const entry of difference) {
         element.innerHTML += `
             <section class="${entry.type}" data-test="message">
