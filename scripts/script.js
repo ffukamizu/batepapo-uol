0ccc22dd3985ccd9df40cd11ba703b77fde2fb28
ffukamizu <f.fukamizu@live.com>
2023-04-14 19:07:42 -0300

    finalized user list

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let userName, lastArray, statusUpdate, msgUpdate;
+let userName, lastArray, lasUserArray, statusUpdate, userUpdate, msgUpdate;
 
 const userNameAdress = document.getElementById("user-name");
 const messageAdress = document.getElementById("user-text");
@@ -21,18 +21,6 @@ class Message {
   }
 }
 
-function welcomeScreen() {
-  document.querySelector(".welcome-screen").classList.add("welcome-screen-display");
-}
-
-function welcomeScreenError() {
-  document.querySelector(".error-message").classList.add("error-message-display");
-}
-
-function sideMenu() {
-  document.querySelector(".backdrop").classList.toggle("backdrop-display");
-}
-
 function logIn() {
   userName = String(userNameAdress.value);
 
@@ -43,6 +31,7 @@ function logIn() {
   promise.then(welcomeScreen);
   promise.then(userStatus);
   promise.then(messageHistory);
+  promise.then(userList);
   promise.then(intervalUpdates);
 
   promise.catch(welcomeScreenError);
@@ -56,22 +45,24 @@ function resetLogIn() {
   window.location.reload();
 }
 
+function intervalUpdates() {
+  statusUpdate = setInterval(userStatus, 5000);
+  userUpdate = setInterval(userListUpdate, 3000);
+  msgUpdate = setInterval(messageHistoryUpdate, 3000);
+}
+
+function intervalCancel() {
+  clearInterval(statusUpdate);
+  clearInterval(userUpdate);
+  clearInterval(msgUpdate);
+}
+
 function userStatus() {
   const name = { name: userName };
 
   const promise = axios.post(apiStatus, name);
 
   promise.catch(intervalCancel);
-}
-
-function intervalUpdates() {
-  statusUpdate = setInterval(userStatus, 5000);
-  msgUpdate = setInterval(messageHistoryUpdate, 3000);
-}
-
-function intervalCancel() {
-  clearInterval(statusUpdate);
-  clearInterval(msgUpdate);
 }
 
 function messageUser() {
@@ -96,15 +87,19 @@ function messageHistory() {
 
     for (const entry of array.data) {
       element.innerHTML += `
-            <section class="${entry.type}" data-test="message">
-              <p class="time-sent">(${entry.time})</p>
-              <p class="user-message">${entry.from} para ${entry.to}: ${entry.text}</p>
-            </section>
-            `;
+      <li class="${entry.type}" data-test="message">
+        <p class="time-sent">(${entry.time})</p>
+        <p class="user-message">${entry.from} para ${entry.to}: ${entry.text}</p>
+      </li>
+      `;
     }
 
     element.lastElementChild.scrollIntoView();
   }
+}
+
+function messageDestination(userId) {
+  console.log(userId);
 }
 
 function messageHistoryUpdate() {
@@ -117,16 +112,16 @@ function messageHistoryUpdate() {
   function getLastMessageContent(array) {
     const currentArray = array.data;
 
-    const difference = currentArray.filter((n) => !lastArray.includes(n));
-
     if (JSON.stringify(lastArray) !== JSON.stringify(array.data)) {
-      for (const entry of difference) {
+      element.innerHTML = "";
+
+      for (const entry of currentArray) {
         element.innerHTML += `
-            <section class="${entry.type}" data-test="message">
-              <p class="time-sent">(${entry.time})</p>
-              <p class="user-message">${entry.from} para ${entry.to}: ${entry.text}</p>
-            </section>
-            `;
+        <li class="${entry.type}" data-test="message">
+          <p class="time-sent">(${entry.time})</p>
+          <p class="user-message">${entry.from} para ${entry.to}: ${entry.text}</p>
+        </li>
+        `;
       }
 
       lastArray = currentArray;
@@ -136,6 +131,77 @@ function messageHistoryUpdate() {
 
     element.lastElementChild.scrollIntoView();
   }
+}
+
+function userList() {
+  const element = document.querySelector("#user-list");
+
+  const promise = axios.get(apiUser);
+
+  promise.then(getUserList);
+
+  function getUserList(array) {
+    element.innerHTML = ` 
+      <div class="contact-selection" onclick="messageDestination('Todos')" data-test="all">
+        <ion-icon name="people"></ion-icon><button>Todos</button>
+        <div class="checkmark checkmark-hide"><ion-icon name="checkmark-outline"></ion-icon></div>
+      </div>`;
+
+    for (const entry of array.data) {
+      element.innerHTML += `
+      <div onclick="messageDestination('${entry.name}')" data-test="participant">
+        <ion-icon name="person-circle"></ion-icon><button>${entry.name}</button>
+        <div class="checkmark checkmark-hide"><ion-icon name="checkmark-outline"></ion-icon></div>
+      </div>
+      `;
+    }
+  }
+}
+
+function userListUpdate() {
+  const element = document.querySelector("#user-list");
+
+  const promise = axios.get(apiUser);
+
+  promise.then(getUserList);
+
+  function getUserList(array) {
+    if (JSON.stringify(lastUserArray) !== JSON.stringify(array.data)) {
+      const currentArray = array.data;
+
+      element.innerHTML = ` 
+      <div class="contact-selection" onclick="messageDestination('Todos')" data-test="all">
+        <ion-icon name="people"></ion-icon><button>Todos</button>
+        <div class="checkmark checkmark-hide"><ion-icon name="checkmark-outline"></ion-icon></div>
+      </div>
+      `;
+
+      for (const entry of currentArray) {
+        element.innerHTML += `
+        <div onclick="messageDestination('${entry.name}')" data-test="participant">
+          <ion-icon name="person-circle"></ion-icon><button>${entry.name}</button>
+          <div class="checkmark checkmark-hide"><ion-icon name="checkmark-outline"></ion-icon></div>
+        </div>
+        `;
+      }
+
+      lastUserArray = currentArray;
+    } else {
+      null;
+    }
+  }
+}
+
+function welcomeScreen() {
+  document.querySelector(".welcome-screen").classList.add("welcome-screen-display");
+}
+
+function welcomeScreenError() {
+  document.querySelector(".error-message").classList.add("error-message-display");
+}
+
+function sideMenu() {
+  document.querySelector(".backdrop").classList.toggle("backdrop-display");
 }
 
 userNameAdress.addEventListener("keypress", function (event) {
